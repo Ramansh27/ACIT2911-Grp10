2260377ced307841a4d56d3cac20750c50f1bae5
Ramansh27 <rchhabra6@my.bcit.ca>
2022-05-26 20:57:02 -0800

    .

--- functions.py
+++ functions.py
@@ -11,7 +11,7 @@ login_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'admi
 transactions_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'admin-only', 'transactions.json')
 
 def read_products():
-    with open(os.path.join(products_file_path, 'products.csv'), 'r') as file:
+    with open(os.path.join(products_file_path, 'products.csv'), 'r', encoding='windows-1252') as file:
             product_list = list(csv.reader(file))
             images = os.listdir('static/product_image')
             image_list = []
